b0da0b2ed5ce36ae109e9a9c7d5df28f196535ee
Camilo Lopera <loperanartinezjuancamilo@gmail.com>
2023-09-13 13:48:42 -0500

    seccion 6 terminada

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -10,10 +10,15 @@ export class InfoPaginaService {
   info: InfoPagina = {};
   cargada = false;
 
+  equipo: any[] = [];
+
   constructor( private http: HttpClient) { 
 
-    //console.log('Servicio de infoPagina listo')
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
 
+  private cargarInfo(){
     //Leer archivo JSON 
     this.http.get('assets/data/data-pagina.json')
       .subscribe( (resp: InfoPagina) => { 
@@ -21,9 +26,18 @@ export class InfoPaginaService {
         this.cargada = true;
         this.info = resp;
 
-        console.log(resp);
+        //console.log(resp);
       });
-
-
   }
+
+  private cargarEquipo(){
+    this.http.get('https://angular-html-69cf3-default-rtdb.firebaseio.com/equipo.json')
+    .subscribe( (resp: any) => { 
+
+      this.equipo = resp;
+      console.log(resp);
+    });
+  }
+
+
 }
---
+++ src/app/services/productos.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Producto } from '../interfaces/producto.interface';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductosService {
+
+  cargando = true;
+  productos: any;
+
+  constructor(private http: HttpClient) { 
+    this.cargarProductos();
+  }
+
+  private cargarProductos(){
+    this.http.get('https://angular-html-69cf3-default-rtdb.firebaseio.com/productos_idx.json')
+    .subscribe( (resp: Producto) =>{
+
+      console.log(resp);
+      this.productos = resp;
+      this.cargando = false;
+    }
+    );
+  }
+
+}
