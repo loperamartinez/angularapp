d15de31709786b2187cc20979b9f9a9eb1ff4674
Camilo Lopera <loperanartinezjuancamilo@gmail.com>
2023-09-14 19:27:55 -0500

    seccion 7

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -10,20 +10,64 @@ export class ProductosService {
 
   cargando = true;
   productos: any;
+  productosFiltrado: any[] = [];
 
   constructor(private http: HttpClient) { 
     this.cargarProductos();
   }
 
   private cargarProductos(){
-    this.http.get('https://angular-html-69cf3-default-rtdb.firebaseio.com/productos_idx.json')
-    .subscribe( (resp: Producto) =>{
 
-      console.log(resp);
-      this.productos = resp;
-      this.cargando = false;
+    return new Promise <void>( ( resolve, reject ) => {
+      this.http.get('https://angular-html-69cf3-default-rtdb.firebaseio.com/productos_idx.json')
+      .subscribe( (resp: Producto) =>{
+  
+        this.productos = resp;
+        this.cargando = false;
+        resolve();
+      }
+      );
+    })
+    
+  }
+
+  getProducto( id: string){
+
+    return this.http.get(`https://angular-html-69cf3-default-rtdb.firebaseio.com/productos/${ id }.json`);
+
+  }
+
+  buscarProducto( termino: string){
+
+    if( this.productos.length === 0){
+      //cargar productos
+      this.cargarProductos().then( () => {
+        // ejecutar después de tener los productos
+        // aplicar el filtro
+        this.filtrarProductos( termino );
+      })
     }
-    );
+    else{
+      // aplicar el filtro
+      this.filtrarProductos( termino );
+    }
+  }
+
+  private filtrarProductos( termino: string){
+    console.log(this.productos);
+    this.productosFiltrado = [];
+
+    //Para que no importe si se escribe en mayuscula o minuscula
+    //termino = termino.toLocaleLowerCase();
+
+    this.productos.forEach( (prod:any) => {
+
+      //const tituloLower = prod.titulo.toLocalLowerCase();
+
+      if( prod.categoria.indexOf( termino ) >= 0 || prod.titulo.indexOf( termino ) >= 0) {
+        this.productosFiltrado.push( prod );
+      }
+    });
   }
 
 }
